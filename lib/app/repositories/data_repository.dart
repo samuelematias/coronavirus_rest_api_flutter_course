2cda7923ce8cedaa930fcaa2c014161de342f02c
samuelematias <hi@samuelematias.com>
2020-07-27 12:40:39 -0300

    feat(lib): add-provider-package-dark-theme

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,6 +1,7 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   DataRepository({@required this.apiService});
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider<DataRepository>(
+      create: (_) => DataRepository(
+        apiService: APIService(API.sandbox()),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Coronavirus Tracker',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xFF101010),
+          cardColor: Color(0xFF222222),
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
