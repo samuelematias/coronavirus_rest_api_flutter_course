2aa6588c3dd6622a2f6683ab260edba5af08965a
samuelematias <hi@samuelematias.com>
2020-07-27 12:15:12 -0300

    feat(token): data-repository-refresh-access-token

--- lib/app/repositories/data_ repository.dart
+++ lib/app/repositories/data_ repository.dart
@@ -6,9 +6,23 @@ class DataRepository {
   DataRepository({@required this.apiService});
   final APIService apiService;
 
+  String _accessToken;
+
   Future<int> getEndpointData(Endpoint endpoint) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(
-        accessToken: accessToken, endpoint: endpoint);
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      // if unauthorized, get access token again
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
   }
 }
