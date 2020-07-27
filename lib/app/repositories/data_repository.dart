45970b4116f268b2260277a4ec61bcb5e045b120
samuelematias <hi@samuelematias.com>
2020-07-27 13:36:24 -0300

    feat(async): future.wait

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,5 +1,5 @@
-import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
@@ -25,5 +25,22 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<void> _getAllEndpointsData() async {
+    //All futures execute in parallel (concurrently)
+    //When all futures have completed, result is returned
+    await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
   }
 }
