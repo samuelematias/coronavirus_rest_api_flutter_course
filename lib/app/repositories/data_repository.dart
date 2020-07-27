3e4f7b0ba82f124fb25ee00522bee9f2affbcee7
samuelematias <hi@samuelematias.com>
2020-07-27 13:42:57 -0300

    feat(model): add-endpoints-data

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:flutter/foundation.dart';
@@ -27,10 +28,11 @@ class DataRepository {
     }
   }
 
-  Future<void> _getAllEndpointsData() async {
+  Future<EndpointsData> _getAllEndpointsData() async {
     //All futures execute in parallel (concurrently)
     //When all futures have completed, result is returned
-    await Future.wait([
+    //This returns a List<int>
+    final values = await Future.wait([
       apiService.getEndpointData(
           accessToken: _accessToken, endpoint: Endpoint.cases),
       apiService.getEndpointData(
@@ -42,5 +44,14 @@ class DataRepository {
       apiService.getEndpointData(
           accessToken: _accessToken, endpoint: Endpoint.recovered),
     ]);
+    return EndpointsData(
+      values: {
+        Endpoint.cases: values[0],
+        Endpoint.casesSuspected: values[1],
+        Endpoint.casesConfirmed: values[2],
+        Endpoint.deaths: values[3],
+        Endpoint.recovered: values[4],
+      },
+    );
   }
 }
