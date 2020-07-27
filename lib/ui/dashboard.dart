c806e8a10be14c3de66e1c78ee47b0cb99e28a39
samuelematias <hi@samuelematias.com>
2020-07-27 18:20:54 -0300

    fix(data-null): hotfix-null-errors

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -51,7 +51,7 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     final formatter = LastUpdatedDateFormatter(
       lastUpdated: _endpointsData != null
-          ? _endpointsData.values[Endpoint.cases].date
+          ? _endpointsData.values[Endpoint.cases]?.date
           : null,
     );
 
@@ -70,7 +70,7 @@ class _DashboardState extends State<Dashboard> {
               EndpointCard(
                 endpoint: endpoint,
                 value: _endpointsData != null
-                    ? _endpointsData.values[endpoint].value
+                    ? _endpointsData.values[endpoint]?.value
                     : null,
               ),
           ],
